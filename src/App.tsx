28b8db4807a4ee405e0144545623bf46410f3fd4
unknown <afaridesmond@gmail.com>
2024-01-19 20:27:09 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Index from "./components/Index"
+
+function App() {
+  return (
+    <div className="p-4 md:p-6 min-h-screen px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0 bg-zinc-900 text-zinc-500">
+      <Index />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+const Index: FC = () => {
+  return (
+    <div className='mx-auto max-w-screen-xl'>
+      <p>Hi 👋, my name is</p>
+      <h1 className='text-4xl font-bold text-zinc-200 sm:text-5xl'>Desmond Afari</h1>
+      <h2 className='mt-3 text-lg font-medium text-zinc-200 sm:text-xl'>Machine Learning Engineer</h2>
+      <p className='mt-4'>I harness the power of data and cutting-edge algorithms to develop solutions.</p>
+    </div>
+  )
+}
+
+export default Index

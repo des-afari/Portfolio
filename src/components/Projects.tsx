fb0b24c74d849a06e4d8d56f86ad43b4768d832c
unknown <afaridesmond@gmail.com>
2024-01-21 19:31:38 +0000

    projects edited

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,22 +27,34 @@ const Projects: FC = () => {
           </div>
         </a> 
         <a href="https://desmonda-movie-recommendation-system.hf.space/" target='_blank'>
-          <div className='lg:grid lg:grid-cols-4 lg:gap-x-3 space-y-2 lg:space-y-0 px-3 py-4 lg:py-6 bg-zinc-800 border border-zinc-700'>
+          <div className='lg:grid lg:grid-cols-4 lg:gap-x-3 space-y-2 lg:space-y-0 px-3 py-4 lg:py-6 bg-zinc-800 border border-b-0 border-zinc-700'>
             <img src={recImage} className='col-span-1 w-[55%] lg:w-full lg:mt-1' alt="recommender project image" />
             <div className='col-span-3 pt-2 lg:pt-0 lg:space-y-1'>
-              <h3 className='text-zinc-200 font-semibold text-lg lg:text-xl lg:font-bold'>Movie Recommendation App</h3>
+              <h1 className='text-zinc-200 font-semibold text-lg lg:text-xl lg:font-bold'>Movie Recommendation App</h1>
               <p className='lg:text-base'>Recommends ten movies you might be interested in watching based on your favorite movie.</p>
             </div>
-          </div>        
+          </div>
+          <div className='text-xs p-3 flex gap-2 flex-wrap bg-zinc-800 border border-t-0 border-zinc-700'>
+            <span className='items-center rounded-md border border-zinc-500 px-2.5 py-0.5 text-xs font-semibold'>Streamlit</span>
+            <span className='items-center rounded-md border border-zinc-500 px-2.5 py-0.5 text-xs font-semibold'>Pickle</span>
+            <span className='items-center rounded-md border border-zinc-500 px-2.5 py-0.5 text-xs font-semibold'>Scikit Learn</span>
+            <span className='items-center rounded-md border border-zinc-500 px-2.5 py-0.5 text-xs font-semibold'>K Nearest Neighbor</span>
+          </div>       
         </a> 
         <a href="https://pokebook-delta.vercel.app/" target='_blank'>
-          <div className='lg:grid lg:grid-cols-4 lg:gap-x-3 space-y-2 lg:space-y-0 px-3 py-4 lg:py-6 bg-zinc-800 border border-zinc-700'>
+          <div className='lg:grid lg:grid-cols-4 lg:gap-x-3 space-y-2 lg:space-y-0 px-3 py-4 lg:py-6 bg-zinc-800 border border-b-0 border-zinc-700'>
             <img src={pokeImage} className='col-span-1 w-[55%] lg:w-full lg:mt-1' alt="pokebook project image" />
             <div className='col-span-3 pt-2 lg:pt-0 lg:space-y-1'>
-              <h3 className='text-zinc-200 font-semibold text-lg lg:text-xl lg:font-bold'>PokeBook</h3>
-              <p className='lg:text-base'>A list of all pokemons, their strengths and weaknesses and their stats.</p>
+              <h3 className='text-zinc-200 font-semibold text-lg lg:text-xl lg:font-bold'>Pokebook</h3>
+              <p className='lg:text-base'>A list of all pokemons, their strengths and weaknesses, and their stats.</p>
             </div>
-          </div>        
+          </div>
+          <div className='text-xs p-3 flex gap-2 flex-wrap bg-zinc-800 border border-t-0 border-zinc-700'>
+            <span className='items-center rounded-md border border-zinc-500 px-2.5 py-0.5 text-xs font-semibold'>React</span>
+            <span className='items-center rounded-md border border-zinc-500 px-2.5 py-0.5 text-xs font-semibold'>Axios</span>
+            <span className='items-center rounded-md border border-zinc-500 px-2.5 py-0.5 text-xs font-semibold'>React Paginate</span>
+            <span className='items-center rounded-md border border-zinc-500 px-2.5 py-0.5 text-xs font-semibold'>React Router DOM</span>
+          </div>              
         </a>      
       </div>
     </section>
